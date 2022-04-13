913d13410ba5c9bb06d9c49170a15f616e4c001c
Kaden Wyble <60489116+0Kden@users.noreply.github.com>
2022-04-13 12:51:09 -0500

    Add files via upload

---
+++ Assignment 11 (Serial Com.)/Assignment_3_Serial_Com.ino
@@ -0,0 +1,27 @@
+/* Kaden Wyble
+ * 899240787
+ * Youtube link ---> https://www.youtube.com/shorts/FtffIwDWlAs
+ */
+ const int switchPin = 2; 
+const int ledPin = 4;
+int switchVal = 0; 
+
+void setup() {
+  pinMode(switchPin, INPUT);
+  pinMode(ledPin,OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  int brightness;
+ if (Serial.available()) {
+  brightness = Serial.read();
+  Serial.println(brightness);
+  digitalWrite(ledPin, brightness);
+ }
+ 
+ 
+  switchVal = digitalRead(2);
+  Serial.println(switchVal);
+  delay(10);
+}
---
+++ Assignment 11 (Serial Com.)/js/sketch.js
@@ -0,0 +1,95 @@
+/*  Kaden Wyble
+    899240787
+    Youtube link ---> https://www.youtube.com/shorts/FtffIwDWlAs
+*/
+
+let serial;
+let bright;
+let latestData = "waiting for data";
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+
+  serial = new p5.SerialPort();
+
+  serial.list();
+  serial.open('COM4');
+
+  serial.on('connected', serverConnected);
+
+  serial.on('list', gotList);
+
+  serial.on('data', gotData);
+
+  serial.on('error', gotError);
+
+  serial.on('open', gotOpen);
+
+  serial.on('close', gotClose);
+
+  button = createButton('Let there be Light!!!');
+  button.position(200,200);
+  button.mousePressed(light);
+  button2 = createButton('Darkness!!!');
+  button2.position(220,220);
+  button2.mousePressed(dark);
+  
+}
+
+function light() {
+  bright = 225;
+  serial.write(bright);
+  console.log(bright);
+}
+
+function dark() {
+  bright = 0;
+  serial.write(bright);
+  console.log(bright);
+}
+
+
+
+function serverConnected() {
+  print("Connected to Server");
+}
+
+function gotList(thelist) {
+  print("List of Serial Ports:");
+
+  for (let i = 0; i < thelist.length; i++) {
+    print(i + " " + thelist[i]);
+  }
+}
+
+function gotOpen() {
+  print("Serial Port is Open");
+}
+
+function gotClose(){
+  print("Serial Port is Closed");
+  latestData = "Serial Port is Closed";
+}
+
+function gotError(theerror) {
+  print(theerror);
+}
+
+function gotData() {
+  let currentString = serial.readLine();
+    trim(currentString);
+  if (!currentString) return;
+  console.log(currentString);
+  latestData = currentString;
+}
+
+function draw() {
+  if (latestData == 0) {
+    background(255,100,100);
+  }
+  else { // if it is pressed, we get a 1
+    background(100,100,255);
+  }
+  fill(0,0,0);
+  text(latestData, 10, 20);
+}
